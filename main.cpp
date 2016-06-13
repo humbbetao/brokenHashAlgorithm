e48117e95574cac4454a09e520d796f41fa46dd2
humbbetao <humberto_voleibol@hotmail.com>
2016-06-12 22:20:34 -0300

    baixei os codigo da hash sha 256, sha1, md5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include "sha1.h"
+#include "sha256.h"
+#include "md5.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    string input = "humb8657naval";
+    string output1 = sha256(input);
+
+    cout << "sha256('"<< input << "'):" << output1 << endl;
+    cout << "sha1('grape'):" << sha1("humb8657naval") << endl;
+    cout << "md5 of 'grape': " << md5("humb8657naval") << endl;
     return 0;
 }
+
+
+
