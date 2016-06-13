5c1ab2cb6370e13bfb172b650eeceaefaa379e08
humbbetao <humberto_voleibol@hotmail.com>
2016-06-13 20:42:00 -0300

    baixei as wordlist dois melhoradas

--- main.cpp
+++ main.cpp
@@ -8,30 +8,44 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     string senha = "99999";
-
+    string nomeDoArquivoDeWordListPortuguese = "dictionary_brazilian.dic";
+    string nomeDoArquivoDeWordListEnglish = "dictionary_english.dic";
     string saidaDoMd5 = md5(senha);
     string saidaDoSha1 =sha1(senha);
-     string saidaDoSha256 = sha256(senha);
+    string saidaDoSha256 = sha256(senha);
 
     cout << "md5('"<< senha << "'):" << saidaDoMd5 << endl;
     cout << "sha1('"<< senha << "'):" << saidaDoSha1 << endl;
     cout << "sha256('"<< senha << "'):" << saidaDoSha256 << endl;
 
-    //int i =0;
     int chave = 1;
     int i=0;
     string chaveGerada;
     stringstream resultado;
+    string line;
+    ifstream myfile ("dictionary_brazilian.dic");
+
+    if (myfile.is_open())
+    {
+        while (! myfile.eof() ){ //enquanto end of file for false continua
+                getline (myfile,line); // como foi aberto em modo texto(padrão) e não binário(ios::bin) pega cada linha
+                cout << line << endl;
+            }
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+
+
     while(true)
     {
         i++;
         chave++;
-
         resultado<<i;
         chaveGerada = md5(resultado.str());
         resultado.str("");
@@ -42,7 +56,6 @@ int main(int argc, char *argv[])
             break;
         }
         cout<<"Testando "<< i<<" ChaveGerada "<<chaveGerada<<"\n"<<endl;
-
     }
 
     return 0;
