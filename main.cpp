bd3c6b94c67a0e2fe225f766d58a5e5ab823d1c3
humbbetao <humberto_voleibol@hotmail.com>
2016-06-13 00:35:44 -0300

    o algoritmo ta certo

--- main.cpp
+++ main.cpp
@@ -2,17 +2,49 @@
 #include "sha1.h"
 #include "sha256.h"
 #include "md5.h"
-
+#include <string>
+#include <iomanip>
+#include <locale>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string input = "humb8657naval";
-    string output1 = sha256(input);
+    string senha = "99999";
 
-    cout << "sha256('"<< input << "'):" << output1 << endl;
-    cout << "sha1('grape'):" << sha1("humb8657naval") << endl;
-    cout << "md5 of 'grape': " << md5("humb8657naval") << endl;
+    string saidaDoMd5 = md5(senha);
+    string saidaDoSha1 =sha1(senha);
+     string saidaDoSha256 = sha256(senha);
+
+    cout << "md5('"<< senha << "'):" << saidaDoMd5 << endl;
+    cout << "sha1('"<< senha << "'):" << saidaDoSha1 << endl;
+    cout << "sha256('"<< senha << "'):" << saidaDoSha256 << endl;
+
+    //int i =0;
+    int chave = 1;
+    int i=0;
+    string chaveGerada;
+    stringstream resultado;
+    while(true)
+    {
+        i++;
+        chave++;
+
+        resultado<<i;
+        chaveGerada = md5(resultado.str());
+        resultado.str("");
+        resultado.clear();
+        if(chaveGerada==saidaDoMd5)
+        {
+            cout<<"quebrou"<<endl;
+            break;
+        }
+        cout<<"Testando "<< i<<" ChaveGerada "<<chaveGerada<<"\n"<<endl;
+
+    }
+
     return 0;
 }
 
